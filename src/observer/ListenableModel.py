f4aa810637d796f4d4fd0f398370454710dd5bbc
ValentinLe <valou.leblond1@gmail.com>
2019-09-03 17:05:39 +0200

    doc observer

--- src/observer/ListenableModel.py
+++ src/observer/ListenableModel.py
@@ -1,14 +1,33 @@
 
 class ListenableModel():
+    '''
+    Classe permettant a un model d'etre ecouter par les ecouteurs
+    '''
+
     def __init__(self):
         self.listeners = []
 
     def addListener(self, modelListener):
+        '''
+        ajoute un ecouteur au model
+
+        :param modelListener: l'ecouteur a ajouter pour ecouter le model
+        :type modelListener: observer.ModelListener
+        '''
         self.listeners.append(modelListener)
 
     def removeListener(self, modelListener):
+        '''
+        supprime un ecouteur au model
+
+        :param modelListener: l'ecouteur a retirer du model
+        :type observeur.ModelListener
+        '''
         self.listeners.remove(modelListener)
 
     def firechange(self):
+        '''
+        indique a tous les ecouteurs du model qu'il s'est passe quelque chose
+        '''
         for listener in self.listeners:
             listener.update()
--- src/observer/ModelListener.py
+++ src/observer/ModelListener.py
@@ -1,5 +1,11 @@
 
 class ModelListener():
+    '''
+    Classe qui servira pour les ecouteurs du model afin qu'ils puissent se mettre a jour
+    '''
 
     def update(self):
+        '''
+        indique a l'ecouteur que quelque chose s'est passe dans le model
+        '''
         raise NotImplementedError("method \"update\" isn't implemented")
